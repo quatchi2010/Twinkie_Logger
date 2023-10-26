67696b7f05d4deb9fbceb1d78affebf0f61c950a
James Lee <tengyunlee@google.com>
2023-10-26 14:49:35 +0800

    Format the c code

--- include/libtwinkie_v2.h
+++ include/libtwinkie_v2.h
@@ -1,18 +1,19 @@
 #ifndef _LIBTWINKIE_V2_H_
 #define _LIBTWINKIE_V2_H_
 
-#include "device.h"
 #include <vector>
 
-class LibTwinkieV2
-{
-public:
-	LibTwinkieV2();
-	~LibTwinkieV2();
-	int init();
-	void xray();
-private:
-	std::vector<Device *> devices;
+#include "device.h"
+
+class LibTwinkieV2 {
+ public:
+  LibTwinkieV2();
+  ~LibTwinkieV2();
+  int init();
+  void xray();
+
+ private:
+  std::vector<Device *> devices;
 };
 
-#endif // _LIBTWINKIE_V2_H_
+#endif  // _LIBTWINKIE_V2_H_
--- include/snooper_stream.h
+++ include/snooper_stream.h
@@ -1,13 +1,13 @@
 #ifndef _SNOOPER_STREAM_H_
 #define _SNOOPER_STREAM_H_
 
-#include <stdint.h>
 #include <stdbool.h>
+#include <stdint.h>
 
 void stream_init(void);
-int stream_write(const uint8_t const * data, const uint32_t len);
+int stream_write(const uint8_t const *data, const uint32_t len);
 bool stream_read(uint8_t *buf);
-uint8_t const * stream_get_data(void);
+uint8_t const *stream_get_data(void);
 void stream_reset(void);
 
 #endif /* _SNOOPER_STREAM_H_ */
